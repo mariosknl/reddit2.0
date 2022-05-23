80ae8da4400eea01596495546148f24d98304ae6
mariosknl <mariosknl@outlook.com>
2022-05-23 13:34:32 +0300

    implmenting reddit login using nextauth

--- components/Header.tsx
+++ components/Header.tsx
@@ -15,8 +15,11 @@ import {
   SpeakerphoneIcon,
   VideoCameraIcon,
 } from '@heroicons/react/outline'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 function Header() {
+  const { data: session } = useSession()
+
   return (
     <div className="sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm">
       <div className="relative h-10 w-20 flex-shrink-0 cursor-pointer">
@@ -60,19 +63,47 @@ function Header() {
       </div>
 
       {/* Sign in/Sign out button */}
-      <div className="hidden cursor-pointer items-center space-x-2 border border-gray-100 p-2 lg:flex">
-        <div className="relative h-5 w-5 flex-shrink-0 items-center">
-          <Image
-            src="https://links.papareact.com/23l"
-            alt=""
-            height={5}
-            width={5}
-            layout="fill"
-            objectFit="contain"
-          />
+      {session ? (
+        <div
+          onClick={() => signOut()}
+          className="hidden cursor-pointer items-center space-x-2 border border-gray-100 p-2 lg:flex"
+        >
+          <div className="relative h-5 w-5 flex-shrink-0 items-center">
+            <Image
+              src="https://links.papareact.com/23l"
+              alt=""
+              height={5}
+              width={5}
+              layout="fill"
+              objectFit="contain"
+            />
+          </div>
+
+          <div className="flex-1 text-xs">
+            <p className="truncate">{session?.user?.name}</p>
+            <p className="text-gray-400">1 karma</p>
+          </div>
+
+          <ChevronDownIcon className="h-5 flex-shrink-0 text-gray-400" />
         </div>
-        <p className="text-gray-400">Sign in</p>
-      </div>
+      ) : (
+        <div
+          onClick={() => signIn()}
+          className="hidden cursor-pointer items-center space-x-2 border border-gray-100 p-2 lg:flex"
+        >
+          <div className="relative h-5 w-5 flex-shrink-0 items-center">
+            <Image
+              src="https://links.papareact.com/23l"
+              alt=""
+              height={5}
+              width={5}
+              layout="fill"
+              objectFit="contain"
+            />
+          </div>
+          <p className="text-gray-400">Sign in</p>
+        </div>
+      )}
     </div>
   )
 }

2a5a224e9c41c52d9915a136ca49c25b9864aae8
mariosknl <mariosknl@outlook.com>
2022-05-23 12:52:31 +0300

    building the header component

---
+++ components/Header.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image'
+import React from 'react'
+import { ChevronDownIcon, HomeIcon } from '@heroicons/react/solid'
+import { StarIcon } from '@heroicons/react/outline'
+
+function Header() {
+  return (
+    <div className="sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm">
+      <div className="relative h-10 w-20 flex-shrink-0 cursor-pointer">
+        <Image
+          src="https://links.papareact.com/fqy"
+          objectFit="contain"
+          layout="fill"
+        />
+      </div>
+
+      <div className="mx-7 flex items-center xl:min-w-[300px]">
+        <HomeIcon className="h-5 w-5" />
+        <p className="ml-2 hidden flex-1 lg:inline">Home</p>
+        <ChevronDownIcon className="h-5 w-5" />
+      </div>
+    </div>
+  )
+}
+
+export default Header
